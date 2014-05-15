99936c143c12b7c684723e59f9355cd934b2dc7a
rissole <bonzaibrother@gmail.com>
2014-05-15 22:09:42 +1000

    card + minioncard initial sketch

---
+++ hearthstone.py
@@ -0,0 +1,89 @@
+from enum import Enum
+
+class Card(object):
+    """Common data across all cards"""
+    class Rarity(Enum):
+        """We're using Python for speed of development."""
+        Free = 1
+        Common = 2
+        Rare = 3
+        Epic = 4
+        Legendary = 5
+        
+    class Class(Enum):
+        """We're using Python for speed of development."""
+        Any = 1
+        Druid = 2
+        Hunter = 3
+        Mage = 4
+        Paladin = 5
+        Priest = 6
+        Rogue = 7
+        Shaman = 8
+        Warlock = 9
+        Warrior = 10
+        
+    def __init__(self, mana_cost, name, class_=Class.Any, rarity=None, flavour_text=None, is_golden=False):
+        self.mana_cost = mana_cost
+        self.name = name
+        self.class_ = class_
+        self.rarity = rarity
+        self.flavour_text = flavour_text
+        self.is_golden = is_golden
+    
+    def get_mana_cost(self):
+        return self._mana_cost
+        
+    def get_name(self):
+        return self.name
+        
+    def get_rarity(self):
+        return self.rarity
+        
+    def get_flavour_text(self):
+        return self.flavour_text
+    
+    def get_class(self):
+        """The class that this card belongs to (Druid, Mage, etc)"""
+        return self.class_
+        
+    def is_golden(self):
+        """Not planned for use but included to complete the model"""
+        return self.is_golden
+        
+class MinionCard(Card):
+    """
+    Represents the prototype data for a Minion.
+    When a Minion is summoned, its initial data is loaded from a 
+    MinionCard.
+    """
+    class Race(Enum):
+        """We're using Python for speed of development."""
+        General = 1
+        Beast = 2
+        Demon = 3
+        Dragon = 4
+        Murloc = 5
+        Pirate = 6
+        Totem = 7
+        
+    def __init__(self, mana_cost, name, attack, health, race=Race.General, rarity=None, flavour_text=None):
+        """We're using Python for speed of development."""
+        super().__init__(mana_cost, name, rarity, flavour_text)
+        self.attack = attack
+        self.health = health
+        self.race = race
+        
+    @classmethod
+    def make(cls, iterable):
+        """For loading from JSON/DB"""
+        return cls(*iterable)
+    
+    def get_attack(self):
+        return self.attack
+        
+    def get_health(self):
+        return self.health
+        
+    def get_race(self):
+        return self.race
